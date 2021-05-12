a5ee08b3dae7c5651abeaeec7163826caf291f21
f78ok <f78.okay@gmail.com>
2021-05-12 19:13:34 +0200

    Add redirecting page to Discord

---
+++ assets/js/redirect.js
@@ -0,0 +1,8 @@
+document.getElementById("progress").animate([{ width: "0%" }, { width: "100%" }], { duration: 2500, fill: "forwards", easing: "ease-out" }).play();
+
+setTimeout(function() {
+    if (location.search == "?test") { 
+        return console.log("Test mode is enabled, will not redirect");
+    }
+    location.replace(document.getElementById("target").href);
+}, 2000);
