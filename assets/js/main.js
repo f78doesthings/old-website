9e87c5ccbfa5cdd37a2026775655bca31ba59e5a
f78ok <f78.okay@gmail.com>
2021-05-13 14:23:30 +0200

    Code refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,61 @@
-let theme = localStorage.getItem("pref-appearance-theme") || "neon-auto"
-let custom = localStorage.getItem("pref-appearance-custom")
+// All of this code may be remade in the future
+let theme = localStorage.getItem("pref-appearance-theme") || "neon-auto";
+let custom = localStorage.getItem("pref-appearance-custom");
 
-let time = 0
-let reset = 0
+let time = 0;
+let reset = 0;
 
 if (window.M) M.AutoInit();
 
 function switchTheme(newTheme = theme) {
-    try {
-        if (theme === "custom") $(`#theme-custom`).attr("disabled", true)
-        else $(`link[href*="/assets/css/themes/${theme}.css"]`).attr("disabled", true)
-        if (newTheme === "custom") $("link#theme-custom").attr("disabled", false)
-        else $(`link[href*="/assets/css/themes/${newTheme}.css"]`).attr("disabled", false)
-        $("#-pref-appearance-theme").val(newTheme)
-        if (theme !== newTheme) {
-            theme = newTheme
-            localStorage.setItem("pref-appearance-theme", theme)
-            $("#-pref-appearance-url").val(custom)
-        }
-        if (theme === "custom") {
-            // may be dangerous
-            $("#-pref-appearance-url").attr("disabled", false)
-            $("#-pref-appearance-custom").css("opacity", "1")
-
-            custom = $("#-pref-appearance-url").val()
-            $(`#theme-custom`).attr("href", custom)
-            localStorage.setItem("pref-appearance-custom", custom)
-        } else {
-            $("#-pref-appearance-url")[0].disabled = true
-            $("#-pref-appearance-custom").css("opacity", ".5")
-        }
-    } catch(e) {
-        console.error(e)
+  try {
+    if (theme === "custom") $(`#theme-custom`).attr("disabled", true);
+    else
+      $(`link[href*="/assets/css/themes/${theme}.css"]`).attr("disabled", true);
+    if (newTheme === "custom") $("link#theme-custom").attr("disabled", false);
+    else
+      $(`link[href*="/assets/css/themes/${newTheme}.css"]`).attr(
+        "disabled",
+        false
+      );
+    $("#-pref-appearance-theme").val(newTheme);
+    if (theme !== newTheme) {
+      theme = newTheme;
+      localStorage.setItem("pref-appearance-theme", theme);
+      $("#-pref-appearance-url").val(custom);
     }
+    if (theme === "custom") {
+      // may be dangerous
+      $("#-pref-appearance-url").attr("disabled", false);
+      $("#-pref-appearance-custom").css("opacity", "1");
+
+      custom = $("#-pref-appearance-url").val();
+      $(`#theme-custom`).attr("href", custom);
+      localStorage.setItem("pref-appearance-custom", custom);
+    } else {
+      $("#-pref-appearance-url")[0].disabled = true;
+      $("#-pref-appearance-custom").css("opacity", ".5");
+    }
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-switchTheme()
+switchTheme();
 
 function prefAppearance() {
-    $(".modal#-pref-appearance").modal("open")
+  $(".modal#-pref-appearance").modal("open");
 }
 
-$("#-pref-appearance-theme").change(() => switchTheme($("#-pref-appearance-theme").val()))
-$("#-pref-appearance-url").change(() => switchTheme("custom"))
+$("#-pref-appearance-theme").change(() =>
+  switchTheme($("#-pref-appearance-theme").val())
+);
+$("#-pref-appearance-url").change(() => switchTheme("custom"));
 $("#-pref-appearance-reset").click(() => {
-    reset++
-    if (reset >= 2) {
-        $("#-pref-appearance-reset").html(`<div class="preloader-wrapper small active" style="width: 24px; height: 24px; top: 6px;">
+  reset++;
+  if (reset >= 2) {
+    $("#-pref-appearance-reset")
+      .html(`<div class="preloader-wrapper small active" style="width: 24px; height: 24px; top: 6px;">
             <div class="spinner-layer" style="border-color: #fff;">
                 <div class="circle-clipper left">
                     <div class="circle"></div>
@@ -56,19 +65,19 @@ $("#-pref-appearance-reset").click(() => {
                     <div class="circle"></div>
                 </div>
             </div>
-        </div>`)
-        $("#-pref-appearance-reset-message").text("Resetting...")
-        localStorage.clear() // will change if we add more preferences
-        setTimeout(() => location.reload(), 1000)
-    } else {
-        $("#-pref-appearance-reset").text("Confirm")
-        $("#-pref-appearance-reset-message").text("Tap again to confirm")
-    }
-    setTimeout(() => {
-        if (reset >= 2) return
-        reset = 0
-        $("#-pref-appearance-reset").text("Reset")
-        $("#-pref-appearance-reset-message").text("Reset appearance preferences")
-    }, 3000)
-})
-$(".-pref-appearance").click(prefAppearance)
+        </div>`);
+    $("#-pref-appearance-reset-message").text("Resetting...");
+    localStorage.clear(); // will change if we add more preferences
+    setTimeout(() => location.reload(), 1000);
+  } else {
+    $("#-pref-appearance-reset").text("Confirm");
+    $("#-pref-appearance-reset-message").text("Tap again to confirm");
+  }
+  setTimeout(() => {
+    if (reset >= 2) return;
+    reset = 0;
+    $("#-pref-appearance-reset").text("Reset");
+    $("#-pref-appearance-reset-message").text("Reset appearance preferences");
+  }, 3000);
+});
+$(".-pref-appearance").click(prefAppearance);
--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,8 +1,15 @@
-document.getElementById("progress").animate([{ width: "0%" }, { width: "100%" }], { duration: 2500, fill: "forwards", easing: "ease-out" }).play();
+document
+  .getElementById("progress")
+  .animate([{ width: "0%" }, { width: "100%" }], {
+    duration: 2500,
+    fill: "forwards",
+    easing: "ease-out",
+  })
+  .play();
 
-setTimeout(function() {
-    if (location.search == "?test") { 
-        return console.log("Test mode is enabled, will not redirect");
-    }
-    location.replace(document.getElementById("target").href);
+setTimeout(function () {
+  if (location.search == "?test") {
+    return console.log("Test mode is enabled, will not redirect");
+  }
+  location.replace(document.getElementById("target").href);
 }, 2000);
