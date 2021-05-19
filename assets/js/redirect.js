425824337cd8eb7a312c88d2e856770f88a51314
f78ok <f78.okay@gmail.com>
2021-05-19 15:39:40 +0200

    Minor tweaks and more short links

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,15 +1,17 @@
-document
-  .getElementById("progress")
-  .animate([{ width: "0%" }, { width: "100%" }], {
-    duration: 2500,
-    fill: "forwards",
-    easing: "ease-out",
-  })
-  .play();
+if (Element.prototype.animate) {
+  document
+    .getElementById("progress")
+    .animate([{ width: "0%" }, { width: "100%" }], {
+      duration: 2500,
+      fill: "forwards",
+      easing: "ease-out",
+    })
+    .play();
+}
 
 setTimeout(function () {
-  if (location.search == "?test") {
+  if (window.location.search == "?test") {
     return console.log("Test mode is enabled, will not redirect");
   }
-  location.replace(document.getElementById("target").href);
+  window.location.replace(document.getElementById("target").href);
 }, 2000);
